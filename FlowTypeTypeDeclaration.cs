d02234c0a0ec4150824e83ce55d7b1a7de6e7e28
tihonove <tihonove@skbkontur.ru>
2018-02-10 18:07:08 +0500

    Научил flowtype-генератор работать с генериками

--- FlowTypeTypeDeclaration.cs
+++ FlowTypeTypeDeclaration.cs
@@ -1,12 +1,17 @@
-﻿namespace SKBKontur.Catalogue.FlowType.CodeDom
+namespace SKBKontur.Catalogue.FlowType.CodeDom
 {
     public class FlowTypeTypeDeclaration : FlowTypeStatement
     {
         public string Name { get; set; }
         public FlowTypeType Definition { get; set; }
+        public string[] GenericTypeArguments { get; set; }
 
         public override string GenerateCode(ICodeGenerationContext context)
         {
+            if (GenericTypeArguments != null && GenericTypeArguments.Length > 0)
+            {
+                return string.Format("type {0}<{1}> = {2};", Name, string.Join(", ", GenericTypeArguments), Definition.GenerateCode(context));
+            }
             return string.Format("type {0} = {1};", Name, Definition.GenerateCode(context));
         }
     }
