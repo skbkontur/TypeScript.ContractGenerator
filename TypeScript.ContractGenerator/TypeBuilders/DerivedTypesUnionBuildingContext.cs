807d27be9776ecedf26133897b4d2017bdbe9660
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2019-07-30 12:30:35 +0500

    Add DerivedTypesUnionBuildingContext

---
+++ TypeScript.ContractGenerator/TypeBuilders/DerivedTypesUnionBuildingContext.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Linq;
+
+using SkbKontur.TypeScript.ContractGenerator.CodeDom;
+
+namespace SkbKontur.TypeScript.ContractGenerator.TypeBuilders
+{
+    public class DerivedTypesUnionBuildingContext : TypeBuildingContext
+    {
+        public DerivedTypesUnionBuildingContext(TypeScriptUnit unit, Type type, bool useAbstractChildren = false)
+            : base(unit, type)
+        {
+            this.useAbstractChildren = useAbstractChildren;
+        }
+
+        public override void Initialize(ITypeGenerator typeGenerator)
+        {
+            Declaration = new TypeScriptTypeDeclaration
+                {
+                    Name = Type.Name,
+                    Definition = null
+                };
+            Unit.Body.Add(new TypeScriptExportStatement {Declaration = Declaration});
+        }
+
+        public override bool IsDefinitionBuilt => Declaration.Definition != null;
+
+        public override void BuildDefinition(ITypeGenerator typeGenerator)
+        {
+            var types = Type.Assembly
+                            .GetTypes()
+                            .Where(x => x != Type && Type.IsAssignableFrom(x) && (useAbstractChildren || !x.IsAbstract))
+                            .Select(x => typeGenerator.ResolveType(x).ReferenceFrom(Unit, typeGenerator))
+                            .ToArray();
+            Declaration.Definition = new TypeScriptUnionType(types);
+        }
+
+        private readonly bool useAbstractChildren;
+    }
+}
