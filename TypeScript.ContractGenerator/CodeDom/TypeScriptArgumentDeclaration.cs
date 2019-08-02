2e03d03d940cd78e7c25e05fcfdb32c8f4351ed9
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2019-08-02 12:46:51 +0500

    Optional method argument

--- TypeScript.ContractGenerator/CodeDom/TypeScriptArgumentDeclaration.cs
+++ TypeScript.ContractGenerator/CodeDom/TypeScriptArgumentDeclaration.cs
@@ -3,11 +3,13 @@ namespace SkbKontur.TypeScript.ContractGenerator.CodeDom
     public class TypeScriptArgumentDeclaration : TypeScriptExpression
     {
         public string Name { get; set; }
+        public bool Optional { get; set; }
         public TypeScriptType Type { get; set; }
 
         public override string GenerateCode(ICodeGenerationContext context)
         {
-            return $"{Name}: {Type.GenerateCode(context)}";
+            var optional = Optional ? "?" : "";
+            return $"{Name}{optional}: {Type.GenerateCode(context)}";
         }
     }
 }
