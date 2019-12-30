bc80c87e9012f16138477aaa75883aa04943e209
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2019-12-30 14:27:03 +0500

    Cleanup & fix tests

--- TypeScript.ContractGenerator.Tests/OptionsTests.cs
+++ TypeScript.ContractGenerator.Tests/OptionsTests.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using System.Reflection;
 
 using NUnit.Framework;
 
--- TypeScript.ContractGenerator/TypeBuilders/GenericTypeTypeBuildingContext.cs
+++ TypeScript.ContractGenerator/TypeBuilders/GenericTypeTypeBuildingContext.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 using SkbKontur.TypeScript.ContractGenerator.CodeDom;
 using SkbKontur.TypeScript.ContractGenerator.Extensions;
@@ -10,11 +8,9 @@ namespace SkbKontur.TypeScript.ContractGenerator.TypeBuilders
 {
     public class GenericTypeTypeBuildingContext : ITypeBuildingContext
     {
-        public GenericTypeTypeBuildingContext(Type type, ICustomAttributeProvider customAttributeProvider, TypeScriptGenerationOptions options)
+        public GenericTypeTypeBuildingContext(Type type)
         {
             this.type = type;
-            this.customAttributeProvider = customAttributeProvider;
-            this.options = options;
         }
 
         public bool IsDefinitionBuilt => true;
@@ -26,53 +22,20 @@ namespace SkbKontur.TypeScript.ContractGenerator.TypeBuilders
         public void BuildDefinition(ITypeGenerator typeGenerator)
         {
         }
-//todo add nullable attribute handler
+
         public TypeScriptType ReferenceFrom(TypeScriptUnit targetUnit, ITypeGenerator typeGenerator)
         {
             var typeReference = typeGenerator.ResolveType(type.GetGenericTypeDefinition()).ReferenceFrom(targetUnit, typeGenerator);
             var arguments = new List<TypeScriptType>();
             foreach (var argument in type.GetGenericArguments())
             {
-                var customAttributes = type.GetCustomAttributes();
                 var targetType = typeGenerator.ResolveType(argument).ReferenceFrom(targetUnit, typeGenerator);
-                if (IsTypeNullableMode(customAttributes))
-                {
-                    arguments.Add(targetType);
-                }
-                else
-                {
-                    arguments.Add(targetType is INullabilityWrapperType nullabilityType ? nullabilityType.InnerType : targetType);
-                }
+                arguments.Add(targetType is INullabilityWrapperType nullabilityType ? nullabilityType.InnerType : targetType);
             }
             return new TypeScriptGenericTypeReference(typeReference as TypeScriptTypeReference, arguments.ToArray());
         }
 
-        private bool IsTypeNullableMode(IEnumerable<Attribute> customAttributes)
-        {
-            if (options.NullabilityMode == NullabilityMode.NullableReference)
-            {
-                if (customAttributes is null)
-                {
-                    return false;
-                }
-                var nullableAttribute = customAttributes.SingleOrDefault(a => a.GetType().Name == AnnotationsNames.Nullable);
-                if (nullableAttribute is null)
-                {
-                    return false;
-                }
-                var flags = nullableAttribute.GetType().GetField("NullableFlags").GetValue(nullableAttribute) as byte[];
-
-                if (flags != null)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
         private readonly Type type;
-        private readonly ICustomAttributeProvider customAttributeProvider;
-        private readonly TypeScriptGenerationOptions options;
     }
 
     public class TypeScriptGenericTypeReference : TypeScriptType
