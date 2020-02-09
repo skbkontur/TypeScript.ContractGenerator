18cc037e6466f4ab8d5ba4422fa88fcbf7903961
Pavel Vostretsov <p.vostretsov@skbkontur.ru>
2020-02-09 13:43:30 +0500

    Try to publish all packages simultaneously

--- .fix-refs.cmd
+++
@@ -1,5 +0,0 @@
-dotnet build --force --no-incremental --configuration Release ./TypeScript.ContractGenerator.sln
-
-dotnet ./ProjectRefsFixer/bin/Release/netcoreapp3.1/ProjectRefsFixer.dll --allowPrereleasePackages
-
-pause
