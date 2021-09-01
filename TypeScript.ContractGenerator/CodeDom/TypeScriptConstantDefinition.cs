0f572f379c8775999d957efb277635361690cf57
z.yarin <z.yarin@skbkontur.ru>
2021-09-01 11:27:44 +0500

    added types in variable and constant definition

--- TypeScript.ContractGenerator/CodeDom/TypeScriptConstantDefinition.cs
+++ TypeScript.ContractGenerator/CodeDom/TypeScriptConstantDefinition.cs
@@ -2,18 +2,21 @@ namespace SkbKontur.TypeScript.ContractGenerator.CodeDom
 {
     public class TypeScriptConstantDefinition : TypeScriptStatement
     {
-        public TypeScriptConstantDefinition(string name, TypeScriptExpression value)
+        public TypeScriptConstantDefinition(string name, TypeScriptExpression value, string? typeName = null)
         {
             Name = name;
             Value = value;
+            TypeName = typeName;
         }
 
         public string Name { get; }
         public TypeScriptExpression Value { get; }
+        public string? TypeName { get; }
 
         public override string GenerateCode(ICodeGenerationContext context)
         {
-            return $"const {Name} = {Value.GenerateCode(context)};";
+            var constantType = TypeName == null ? "" : $" : {TypeName}";
+            return $"const {Name}{constantType} = {Value.GenerateCode(context)};";
         }
     }
 }
--- TypeScript.ContractGenerator/CodeDom/TypeScriptVaribaleDefinition.cs
+++ TypeScript.ContractGenerator/CodeDom/TypeScriptVaribaleDefinition.cs
@@ -1,19 +1,22 @@
 namespace SkbKontur.TypeScript.ContractGenerator.CodeDom
 {
-    public class TypeScriptVaribaleDefinition : TypeScriptStatement
+    public class TypeScriptVariableDefinition : TypeScriptStatement
     {
-        public TypeScriptVaribaleDefinition(string name, TypeScriptExpression value)
+        public TypeScriptVariableDefinition(string name, TypeScriptExpression value, string? typeName = null)
         {
             Name = name;
             Value = value;
+            TypeName = typeName;
         }
 
         public string Name { get; }
         public TypeScriptExpression Value { get; }
+        public string? TypeName { get; }
 
         public override string GenerateCode(ICodeGenerationContext context)
         {
-            return $"var {Name} = {Value.GenerateCode(context)};";
+            var variableType = TypeName == null ? "" : $" : {TypeName}";
+            return $"var {Name}{variableType} = {Value.GenerateCode(context)};";
         }
     }
 }
