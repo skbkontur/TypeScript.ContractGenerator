9d12cfb11f13cb913ab7c164866e5c51c6ea7e18
z.yarin <z.yarin@skbkontur.ru>
2021-09-01 11:46:31 +0500

    used TypeScriptType and fixed TS style

--- TypeScript.ContractGenerator/CodeDom/TypeScriptConstantDefinition.cs
+++ TypeScript.ContractGenerator/CodeDom/TypeScriptConstantDefinition.cs
@@ -2,20 +2,20 @@ namespace SkbKontur.TypeScript.ContractGenerator.CodeDom
 {
     public class TypeScriptConstantDefinition : TypeScriptStatement
     {
-        public TypeScriptConstantDefinition(string name, TypeScriptExpression value, string? typeName = null)
+        public TypeScriptConstantDefinition(string name, TypeScriptExpression value, TypeScriptType? type = null)
         {
             Name = name;
             Value = value;
-            TypeName = typeName;
+            Type = type;
         }
 
         public string Name { get; }
         public TypeScriptExpression Value { get; }
-        public string? TypeName { get; }
+        public TypeScriptType? Type { get; }
 
         public override string GenerateCode(ICodeGenerationContext context)
         {
-            var constantType = TypeName == null ? "" : $" : {TypeName}";
+            var constantType = Type == null ? "" : $": {Type.GenerateCode(context)}";
             return $"const {Name}{constantType} = {Value.GenerateCode(context)};";
         }
     }
--- TypeScript.ContractGenerator/CodeDom/TypeScriptVaribaleDefinition.cs
+++ TypeScript.ContractGenerator/CodeDom/TypeScriptVaribaleDefinition.cs
@@ -2,20 +2,20 @@ namespace SkbKontur.TypeScript.ContractGenerator.CodeDom
 {
     public class TypeScriptVariableDefinition : TypeScriptStatement
     {
-        public TypeScriptVariableDefinition(string name, TypeScriptExpression value, string? typeName = null)
+        public TypeScriptVariableDefinition(string name, TypeScriptExpression value, TypeScriptType? type = null)
         {
             Name = name;
             Value = value;
-            TypeName = typeName;
+            Type = type;
         }
 
         public string Name { get; }
         public TypeScriptExpression Value { get; }
-        public string? TypeName { get; }
+        public TypeScriptType? Type { get; }
 
         public override string GenerateCode(ICodeGenerationContext context)
         {
-            var variableType = TypeName == null ? "" : $" : {TypeName}";
+            var variableType = Type == null ? "" : $": {Type.GenerateCode(context)}";
             return $"var {Name}{variableType} = {Value.GenerateCode(context)};";
         }
     }
